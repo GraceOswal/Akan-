c7ea98c671e5628c6370265e65791df0cd14978b
Grace Oswal <graceoswal88@gmail.com>
2020-08-26 17:51:59 +0300

    --- Auto-Git Commit ---

---
+++ script.js
@@ -0,0 +1,93 @@
+alert("Welcome to Ghanian Naming Culture!");
+prompt("When is your birthday"); //
+var age = prompt("Enter you age:");
+age;
+newFunction();
+
+[CC - isthecenturydigits.Forexample1989hasCC = 19
+
+    YY - istheYeardigits(1989 hasYY = 89)
+
+    MM - istheMonth
+
+    DD - istheDayofthemonth
+
+    mod - isthemodulus
+
+    function( % )
+];
+
+const days = [alldaysoftheweekstartingwithSundaytoSaturday];
+
+function newFunction() {
+    var add, dayoftheweek = function(_Dayoftheweek) {}(d) = (((CC / 4) - 2 * CC - 1), +((5 * YY / 4)), +((26 * (MM + 1) / 10)) + DD),
+        mod7;
+    where;
+}
+
+function getAkanName() {
+    var maleAkanName = [Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame]
+    if (Sunday) {
+        console.log("Kwasi");
+    } else if (Monday) {
+        console.log("Kwadwo");
+    } else if (Tuesday) {
+        console.log("Kwabena");
+
+    } else if (Wednesday) {
+        console.log("Kwaku");
+    } else if (Thursday) {
+        console.log("Yaw");
+    } else if (Friday) {
+        console.log("Kofi");
+    } else {
+        console.log("Kwame")
+    }
+}
+const days = [alldaysoftheweekstartingwithSundaytoSaturday];
+
+function getAkanName() {
+    var femaleAkanName = [Akosua, Adowa, Abenaa, Akua, Yaa, Afua, Ama]
+    if (Sunday) {
+        console.log("Akosua");
+    } else if (Monday) {
+        console.log("Adowa");
+    } else if (Tuesday) {
+        console.log("Abenaa");
+
+    } else if (Wednesday) {
+        console.log("Akua");
+    } else if (Thursday) {
+        console.log("Yaa");
+    } else if (Friday) {
+        console.log("Afua");
+    } else {
+        console.log("Ama")
+    }
+}
+
+function isinValidDate(str) {
+    var parts = str.split('/');
+    if (parts.length(d <= 0) return false;
+    }
+    else if (d > 31)
+        return false;
+
+}
+
+function isinValidMonth(str) {
+    var parts = str.split('/');
+    if (parts.length m <= 0)
+        return false;
+} else if (m > 12)
+    return false;
+
+}
+else {
+    results === 0;
+    alert(AkanName);
+}
+
+}
+}
+}
